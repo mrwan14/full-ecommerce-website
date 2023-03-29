d1fb2ef88e583509de4b1cf783fe169ccb5790f4
marwanabdelwahab9@gmail.com <marwanabdelwahab9@gmail.com>
2023-03-29 19:04:07 +0200

    media query

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -4,16 +4,22 @@ import "./login.css";
 export default function Login() {
   return (
     <div>
-      <div className="sign-up ">
+      <div className="login ">
         <div className="row my-5">
           <div className="col-md-6">
+            <div className="login-img">
             <img src={require("../../images/signUp.png")} alt="" />
+
+            </div>
           </div>
           <div className="col-md-6  text-center">
             {" "}
-            <div className="text ">
+            <div className="text  ">
+              <div className="title">
               <h2 className="me-3">Log in to Exclusive</h2>
               <p className="me-3">Enter your details below</p>
+              </div>
+
               <div className="form mt-5 ">
                 <input
                   type="text"
@@ -27,18 +33,15 @@ export default function Login() {
                 />
               </div>
               <div className="d-flex my-5 justify-content-center ms-5 ">
-                <button className="btn login-btn btn-danger me-5 d-flex justify-content-end align-items-start ">
-                  <p className="me-1 mt-0 fs-6">Log In</p>
+                <button className="btn login-btn   ">
+                  <p className="">Log In</p>
                 </button>
-                <Link className="text-danger mt-3">Forget Password?</Link>
+                <Link className="  link">Forget Password?</Link>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-
-      
     </div>
   );
 }
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -8,13 +8,18 @@ export default function SignUp() {
     <div className="sign-up ">
       <div className="row my-5">
         <div className="col-md-6">
-          <img src={require("../../images/signUp.png")} alt="" />
+          <div className="sign-up-img">
+            <img src={require("../../images/signUp.png")} alt="" />
+          </div>{" "}
         </div>
         <div className="col-md-6  text-center">
           {" "}
           <div className="text mt-5">
-            <h2>Create an account</h2>
-            <p>Enter your details below</p>
+            <div className="title">
+              <h2>Create an account</h2>
+              <p>Enter your details below</p>
+            </div>
+
             <div className="form mt-5 ">
               <input
                 type="text"
@@ -31,15 +36,24 @@ export default function SignUp() {
                 placeholder="Password"
                 className=" form-control "
               />
-              <button  className="btn-danger btn">Create Account</button>
-              <button className="btn btn2 text-black d-flex justify-content-center align-items-center ">
-                <h3 className="me-2">
-                  <FcGoogle />
-                </h3>{" "}
-                Sign up with Google
-              </button>
+              <div className="btns">
+                <button className="btn-danger btn mb-3">Create Account</button>
+                <button className="btn btn2 text-black d-flex justify-content-center align-items-center ">
+                  <h3 className="me-2">
+                    <FcGoogle />
+                  </h3>{" "}
+                  Sign up with Google
+                </button>
+              </div>
             </div>
-            <div className="ms-5 my-4"><p className="ms-1">Already have account? <Link to={'/login'} className=" border-bottom ms-2 text-black">Log in</Link></p></div>
+            <div className="ms-4 my-4">
+              <p className="ms-2">
+                Already have account?{" "}
+                <Link to={"/login"} className=" border-bottom ms-2 text-black">
+                  Log in
+                </Link>
+              </p>
+            </div>
           </div>
         </div>
       </div>
